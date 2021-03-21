5fae18f62e13cee063cec2ec13b64f6c713cd044
XY <chelciedealmeida@gmail.com>
2021-03-21 14:39:58 -0700

    cleaned up html and JS files. Completed the Read Me

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# work-day-scheduler
+# **_Work Day Scheduler: Challenge #5_**
+
+
+## **_Objective of the project_**
+The work day scheduler - as the name suggests - allows the user to plan their work day based on a 9AM-5PM schedule. Users can enter their daily tasks and leave the page reassured that their notations have been stored in local storage so any changes made to the scheduler will be saved should the user decide to do so. 
+
+## **_Technologies used_**
+* HTML5
+* CSS3
+* JavaScript
+* JQuery
+* Bootstrap
+* Moment.js
+
+## **_Preview_**
+![Screen Shot 2021-03-21 at 2 15 22 PM](https://user-images.githubusercontent.com/35352010/111921581-8b108880-8a52-11eb-929f-34373aebd6e6.png)
+
+## **_Link URL to deployed app on GitHub_**
+https://chelciedealmeida.github.io/web-api-ch4/https://chelciedealmeida.github.io/work-day-scheduler/
+
+## **_Link URL to GitHub Repository_**
+https://github.com/ChelcieDeAlmeida/work-day-scheduler
--- assets/js/script.js
+++ assets/js/script.js
@@ -16,19 +16,25 @@ $(".saveBtn").on("click", function () {
   localStorage.setItem(timeClock, textData);
 });
 
+// Color coding for time block based on whether the block is in the past //
+// present, or future /
+
 let currentHour = moment().hours();
-console.log(currentHour)
+
+// the '#' denotes that we are iterating through all of the IDs which in this case are on the divs and 
+// which point to individual time blocks which we then traverse to children elements (textarea) to add the class color
+
 for (let i = 9; i <= 17; i++) {
-  $('#' + i).children('textarea').val(localStorage.getItem(i))
+  $('#' + i).children('textarea').val(localStorage.getItem(i)) 
+
   if (i < currentHour) {
     $("#" + i).children('textarea').addClass("past")
+
   } else if (currentHour === i) {
     $("#" + i).children('textarea').addClass("present")
   }
+
   else {
     $("#" + i).children('textarea').addClass("future")
   }
 }
-
-// Color coding for time block based on whether the block is in the past //
-// present, or future /
